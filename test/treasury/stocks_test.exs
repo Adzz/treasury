a1386861db24ee26e47ea18eb465cdfb3b42f515
Adam Lancaster <lancaster28@hotmail.com>
2023-07-02 10:26:49 -0500

    Add some more tests for error cases

--- test/treasury/stocks_test.exs
+++ test/treasury/stocks_test.exs
@@ -34,5 +34,43 @@ defmodule Treasury.StocksTest do
                   refreshed_on: "2 July 2023 @ 03:34"
                 }}
     end
+
+    test "a 500 is an error" do
+      Mox.expect(Treasury.HttpMock, :get, fn request ->
+        assert request == %{
+                 headers: [
+                   {"Authorization", "Bearer cL7FTRzqSfY8CpZXYJ53q2rpV4W5ybvddqObYiODJzc"}
+                 ],
+                 url: "https://treasury.app/api/v1/hiring/symbols/AAA"
+               }
+
+        fixture = "Ahhh"
+
+        {:ok, %{body: fixture, status_code: 500}}
+      end)
+
+      assert Stocks.view_info("AAA") == {:error, "unexpected status code 500"}
+    end
+
+    test "A missing field is an error" do
+      Mox.expect(Treasury.HttpMock, :get, fn request ->
+        assert request == %{
+                 headers: [
+                   {"Authorization", "Bearer cL7FTRzqSfY8CpZXYJ53q2rpV4W5ybvddqObYiODJzc"}
+                 ],
+                 url: "https://treasury.app/api/v1/hiring/symbols/AAA"
+               }
+
+        fixture = "{\"name\":\"Apple Inc\",\"symbol\":\"AAPL\"}"
+
+        {:ok, %{body: fixture, status_code: 200}}
+      end)
+
+      assert Stocks.view_info("AAA") ==
+               {:error,
+                %DataSchema.Errors{
+                  errors: [price: "Field was required but value supplied is considered empty"]
+                }}
+    end
   end
 end
